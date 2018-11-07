33b06cf9fa27b016ad0bd59f5d2525a74e3f713f
nerdelt <nerdelt@gmail.com>
2018-11-06 22:28:47 -0600

    Bonjour

---
+++ Bonjour/Bonjour/Controllers/HelloController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Bonjour.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            string html = "<form method='post' action='/Hello/Display'> +" +
+                "<input type='text' name='name'/>" +
+                "<input type='submit' value='Greet Me!' />" +
+                "</form>";
+
+            return Content(html, "text/html");
+        }
+
+        public IActionResult Display(string name = "World")
+        {
+            return Content(string.Format("<h1>Hello {0}!<h1>", name), "text/html");
+        }
+
+        [Route ("/Hello/Aloha")]
+        public IActionResult Goodbye()
+        {
+            return Content("Goodbye!");
+        }
+    }
+}
